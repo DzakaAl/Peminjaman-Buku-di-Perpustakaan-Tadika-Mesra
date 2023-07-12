d786ddb769b953c83ff86c7f77f8141e50fb19b3
DzakaAl <121382679+DzakaAl@users.noreply.github.com>
2023-07-12 17:36:02 +0700

    Update kritik&saran.php

--- kritik&saran.php
+++ kritik&saran.php
@@ -17,13 +17,13 @@ if(isset($_POST['send'])){
    $number = $_POST['number'];
    $msg = mysqli_real_escape_string($conn, $_POST['message']);
 
-   $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
+   $select_message = mysqli_query($conn, "SELECT * FROM `kritikdansaran` WHERE nama = '$name' AND email = '$email' AND telp = '$number' AND pesan = '$msg'") or die('query failed');
 
    if(mysqli_num_rows($select_message) > 0){
-      $message[] = 'message sent already!';
+      $message[] = 'Pesan telah terikim!';
    }else{
-      mysqli_query($conn, "INSERT INTO `message`(user_id, name, email, number, message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('query failed');
-      $message[] = 'message sent successfully!';
+      mysqli_query($conn, "INSERT INTO `kritikdansaran`(pengguna_id, nama, email, telp, pesan) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('query failed');
+      $message[] = 'Pesan telah terikim!';
    }
 
 }
@@ -36,12 +36,11 @@ if(isset($_POST['send'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>contact</title>
+   <title>Kritik dan Saran</title>
+   <link rel="icon" href="images/perpus.png">
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
@@ -50,19 +49,19 @@ if(isset($_POST['send'])){
 <?php include 'header.php'; ?>
 
 <div class="heading">
-   <h3>contact us</h3>
-   <p> <a href="home.php">home</a> / contact </p>
+   <h3>Kritik dan Saran</h3>
+   <p> <a href="beranda.php">Beranda</a> / Kritik dan Saran </p>
 </div>
 
 <section class="contact">
 
    <form action="" method="post">
-      <h3>say something!</h3>
-      <input type="text" name="name" required placeholder="enter your name" class="box">
-      <input type="email" name="email" required placeholder="enter your email" class="box">
-      <input type="number" name="number" required placeholder="enter your number" class="box">
-      <textarea name="message" class="box" placeholder="enter your message" id="" cols="30" rows="10"></textarea>
-      <input type="submit" value="send message" name="send" class="btn">
+      <h3>Berikan Pendapatmu!</h3>
+      <input type="text" name="name" required placeholder="Nasukkan Nama Anda" class="box">
+      <input type="email" name="email" required placeholder="Masukkan E-Mail Anda" class="box">
+      <input type="number" name="number" required placeholder="Masukkan Nomer Telpon Anda" class="box">
+      <textarea name="message" class="box" placeholder="Masukkan Kritik ataupun Saran Anda" id="" cols="30" rows="10"></textarea>
+      <input type="submit" value="Kirim pesan" name="send" class="btn">
    </form>
 
 </section>
@@ -76,7 +75,6 @@ if(isset($_POST['send'])){
 
 <?php include 'footer.php'; ?>
 
-<!-- custom js file link  -->
 <script src="js/script.js"></script>
 
 </body>
