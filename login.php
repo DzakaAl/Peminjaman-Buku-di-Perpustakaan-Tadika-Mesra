f4ff09350f84295645f2b72d6c8357924e65cded
DzakaAl <121382679+DzakaAl@users.noreply.github.com>
2023-07-12 17:36:22 +0700

    Update login.php

--- login.php
+++ login.php
@@ -8,22 +8,22 @@ if(isset($_POST['submit'])){
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
 
-   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+   $select_users = mysqli_query($conn, "SELECT * FROM `pengguna` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
    if(mysqli_num_rows($select_users) > 0){
 
       $row = mysqli_fetch_assoc($select_users);
 
-      if($row['user_type'] == 'admin'){
+      if($row['tipe_pengguna'] == 'admin'){
 
-         $_SESSION['admin_name'] = $row['name'];
+         $_SESSION['admin_nama'] = $row['nama'];
          $_SESSION['admin_email'] = $row['email'];
          $_SESSION['admin_id'] = $row['id'];
          header('location:admin_dashboard.php');
 
-      }elseif($row['user_type'] == 'user'){
+      }elseif($row['tipe_pengguna'] == 'pengguna'){
 
-         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['user_nama'] = $row['nama'];
          $_SESSION['user_email'] = $row['email'];
          $_SESSION['user_id'] = $row['id'];
          header('location:beranda.php');
@@ -44,12 +44,11 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>login</title>
+   <title>Login</title>
+   <link rel="icon" href="images/perpus.png">
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
@@ -75,7 +74,7 @@ if(isset($message)){
       <input type="email" name="email" placeholder="Masukkan Email" required class="box">
       <input type="password" name="password" placeholder="Masukkan Password" required class="box">
       <input type="submit" name="submit" value="login" class="btn">
-      <p>Tidak Punya Akun? <a href="register.php">Buat Akun</a></p>
+      <p>Apakah Anda belum memiliki akun? <a href="register.php">Buat Akun</a></p>
    </form>
 
 </div>
