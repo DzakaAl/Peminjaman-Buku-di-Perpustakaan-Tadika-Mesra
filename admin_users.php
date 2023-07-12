8e3036c7225212b8584fe1241bfa27180c046df9
DzakaAl <121382679+DzakaAl@users.noreply.github.com>
2023-07-12 17:33:24 +0700

    Update admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -12,7 +12,7 @@ if(!isset($admin_id)){
 
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
-   mysqli_query($conn, "DELETE FROM `users` WHERE id = '$delete_id'") or die('query failed');
+   mysqli_query($conn, "DELETE FROM `pengguna` WHERE id = '$delete_id'") or die('query failed');
    header('location:admin_users.php');
 }
 
@@ -24,12 +24,11 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Pengguna</title>
+   <title>Akun Pengguna</title>
+   <link rel="icon" href="images/perpus.png">
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
 
 </head>
@@ -43,14 +42,14 @@ if(isset($_GET['delete'])){
 
    <div class="box-container">
       <?php
-         $select_users = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+         $select_users = mysqli_query($conn, "SELECT * FROM `pengguna`") or die('query failed');
          while($fetch_users = mysqli_fetch_assoc($select_users)){
       ?>
       <div class="box">
          <p> user id : <span><?php echo $fetch_users['id']; ?></span> </p>
-         <p> username : <span><?php echo $fetch_users['name']; ?></span> </p>
+         <p> username : <span><?php echo $fetch_users['nama']; ?></span> </p>
          <p> email : <span><?php echo $fetch_users['email']; ?></span> </p>
-         <p> user type : <span style="color:<?php if($fetch_users['user_type'] == 'admin'){ echo 'var(--orange)'; } ?>"><?php echo $fetch_users['user_type']; ?></span> </p>
+         <p> user type : <span style="color:<?php if($fetch_users['tipe_pengguna'] == 'admin'){ echo 'var(--orange)'; } ?>"><?php echo $fetch_users['tipe_pengguna']; ?></span> </p>
          <a href="admin_users.php?delete=<?php echo $fetch_users['id']; ?>" onclick="return confirm('Hapus Akun Ini?');" class="delete-btn">Hapus Akun</a>
       </div>
       <?php
@@ -60,15 +59,6 @@ if(isset($_GET['delete'])){
 
 </section>
 
-
-
-
-
-
-
-
-
-<!-- custom admin js file link  -->
 <script src="js/admin_script.js"></script>
 
 </body>
