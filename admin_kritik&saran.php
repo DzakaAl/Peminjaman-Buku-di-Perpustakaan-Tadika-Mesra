3f0099c5b3e129cc8e53bc6ebec343ec99172e9c
DzakaAl <121382679+DzakaAl@users.noreply.github.com>
2023-07-12 17:32:45 +0700

    Update admin_kritik&saran.php

--- admin_kritik&saran.php
+++ admin_kritik&saran.php
@@ -12,8 +12,8 @@ if(!isset($admin_id)){
 
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
-   mysqli_query($conn, "DELETE FROM `message` WHERE id = '$delete_id'") or die('query failed');
-   header('location:admin_kritik&saran.php');
+   mysqli_query($conn, "DELETE FROM `kritikdansaran` WHERE id = '$delete_id'") or die('query failed');
+   header('location:admin_kritikdansaran.php');
 }
 
 ?>
@@ -24,12 +24,11 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Kritik&Saran</title>
+   <title>Kritik dan Saran</title>
+   <link rel="icon" href="images/perpus.png">
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
 
 </head>
@@ -39,42 +38,33 @@ if(isset($_GET['delete'])){
 
 <section class="messages">
 
-   <h1 class="title"> Kritik&saran </h1>
+   <h1 class="title"> Pesan </h1>
 
    <div class="box-container">
    <?php
-      $select_message = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+      $select_message = mysqli_query($conn, "SELECT * FROM `kritikdansaran`") or die('query failed');
       if(mysqli_num_rows($select_message) > 0){
          while($fetch_message = mysqli_fetch_assoc($select_message)){
       
    ?>
    <div class="box">
-      <p> user id : <span><?php echo $fetch_message['user_id']; ?></span> </p>
-      <p> Nama : <span><?php echo $fetch_message['name']; ?></span> </p>
-      <p> No. Tlp : <span><?php echo $fetch_message['number']; ?></span> </p>
-      <p> Email : <span><?php echo $fetch_message['email']; ?></span> </p>
-      <p> Kritik&saran : <span><?php echo $fetch_message['message']; ?></span> </p>
-      <a href="admin_contacts.php?delete=<?php echo $fetch_message['id']; ?>" onclick="return confirm('Hapus Kritik&saran?');" class="delete-btn">Hapus</a>
+      <p> ID Pengguna : <span><?php echo $fetch_message['pengguna_id']; ?></span> </p>
+      <p> Nama : <span><?php echo $fetch_message['nama']; ?></span> </p>
+      <p> Nomer Telpon : <span><?php echo $fetch_message['telp']; ?></span> </p>
+      <p> E-Mail : <span><?php echo $fetch_message['email']; ?></span> </p>
+      <p> Kritik dan Saran : <span><?php echo $fetch_message['pesan']; ?></span> </p>
+      <a href="admin_kritikdansaran.php?delete=<?php echo $fetch_message['id']; ?>" onclick="return confirm('Hapus Pesan ini?');" class="delete-btn">Hapus</a>
    </div>
    <?php
       };
    }else{
-      echo '<p class="empty">Tidak Ada Kritik&saran!</p>';
+      echo '<p class="empty">Tidak Ada kritik dan Saran!</p>';
    }
    ?>
    </div>
 
 </section>
 
-
-
-
-
-
-
-
-
-<!-- custom admin js file link  -->
 <script src="js/admin_script.js"></script>
 
 </body>
